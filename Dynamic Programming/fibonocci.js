472901ebbe5469ecc14645995b1ecc4e6ab22fdb
Nyuydine Bill leynyuy <121938133+NyuydineBill@users.noreply.github.com>
2023-07-09 22:52:30 +0100

    Add files via upload

---
+++ Dynamic Programming/fibonocci.js
@@ -0,0 +1,14 @@
+//Memoization
+
+const fib = (n, memo = {}) => {
+    if (n in memo) return memo[n];
+
+    if (n <=2 ) return 1;
+    memo[n] = fib(n-1,memo) + fib(n-2,memo)
+    return memo[n];
+    
+}
+
+console.log(fib(10))
+console.log(fib(7))
+console.log(fib(1))
